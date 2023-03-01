e6adc3c7b058e99bfa4585a32189542e9298a4cd
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-03-01 19:44:10 -0300

    Adicionar arquivos de projeto.

---
+++ target-sistemas/Program.cs
@@ -0,0 +1,8 @@
+﻿using target_sistemas.questao2;
+
+Console.Write("Digite um termo para sequência: ");
+int n = int.Parse(Console.ReadLine());
+
+Fibonacci f = new();
+
+f.FindValueFibo(n);
---
+++ target-sistemas/questao2/Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace target_sistemas.questao2
+{
+    public class Fibonacci
+    {
+        public int Fibo(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibo(n - 1) + Fibo(n - 2);
+            }
+        }
+
+        public void FindValueFibo(int value)
+        {
+            for(int i = 0; i < value; value--)
+            {
+                if(value == Fibo(value))
+                {
+                    Console.WriteLine($"O valor {value} pertence à sequência");
+                } else
+                {
+                    Console.WriteLine($"O valor {value} não pertence à sequência");
+                }
+            }
+        }
+    }
+
+
+}
