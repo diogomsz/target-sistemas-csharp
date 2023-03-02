5a03c57ca28a88ce634ad8156a81610213c948a7
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-03-02 17:53:21 -0300

    calcula faturamento #2

--- target-sistemas/Program.cs
+++ target-sistemas/Program.cs
@@ -1,10 +1,11 @@
 ﻿using target_sistemas.questao2;
+using target_sistemas.questao3;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-
+        FaturamentoDiario.Ler();
     }
 
     public static void Q1()
---
+++ target-sistemas/questao3/FaturamentoDiario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace target_sistemas.questao3
+{
+    public class FaturamentoDiario
+    {
+        public static void Ler()
+        {
+            string fileName = "C:\\Users\\diogo\\OneDrive\\Ambiente de Trabalho\\project_dev\\dotnet_project\\target-sistemas\\target-sistemas\\dados.json";
+            using StreamReader stream = new(fileName);
+            string json = stream.ReadToEnd();
+            List<Faturamento>? faturamento = JsonSerializer.Deserialize<List<Faturamento>>(json);
+
+            double fMenor = faturamento.MinBy(x => x.valor).valor;
+            Console.WriteLine($"Menor valor de faturamento: {fMenor:c}");
+
+            double fMaior = faturamento.MaxBy(x => x.valor).valor;
+            Console.WriteLine($"Maior valor de faturamento: {fMaior:c}");
+
+            int dia = 0;
+            double fMedia = faturamento.Sum(x => x.valor) / faturamento.Count;
+
+            faturamento.ForEach(x =>
+            {
+                if(x.valor > fMedia)
+                {
+                    dia++;
+                }
+            });
+
+            Console.WriteLine($"Quantidade de dias onde o valor é superior a média mensal: {dia}");
+        }
+    }
+}
